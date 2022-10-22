c56e0f8d5ee3cf557571063bd56b61e49871ca0e
Brijbhna Chauhan <brijphy@gmail.com>
2022-10-22 18:21:02 +0530

    remove lower timeout for api hit

--- src/AxiosInstance/index.js
+++ src/AxiosInstance/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const Axios =  axios.create({
 	   baseURL: 'https://foryoucomputer-backendv2.onrender.com',
-      timeout: 1000,
+      // timeout: 1000,
       headers: {
       	'Content-Type': 'application/json',
     	Authorization: `Bearer ${JSON.parse(localStorage.getItem('userInfo'))?.user_token?.token}`,
